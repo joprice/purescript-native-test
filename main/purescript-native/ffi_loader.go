ee7ea42ed8548bd96c805bf64696c0406507511a
Joseph Price <pricejosephd@gmail.com>
2020-10-09 10:59:27 -0400

    replace node elements in parser

--- main/purescript-native/ffi_loader.go
+++ main/purescript-native/ffi_loader.go
@@ -5,27 +5,46 @@ package ffi_loader
 
 import (
 	_ "github.com/purescript-native/go-ffi/purescript-arrays"
-	_ "github.com/purescript-native/go-ffi/purescript-console"
 	_ "github.com/purescript-native/go-ffi/purescript-effect"
 	_ "github.com/purescript-native/go-ffi/purescript-foldable-traversable"
 	_ "github.com/purescript-native/go-ffi/purescript-globals"
 	_ "github.com/purescript-native/go-ffi/purescript-integers"
+	_ "github.com/purescript-native/go-ffi/purescript-lazy"
 	_ "github.com/purescript-native/go-ffi/purescript-math"
 	_ "github.com/purescript-native/go-ffi/purescript-prelude"
-	_ "github.com/purescript-native/go-ffi/purescript-random"
-	_ "github.com/purescript-native/go-ffi/purescript-unfoldable"
-	_ "github.com/purescript-native/go-ffi/purescript-unsafe-coerce"
-	// for Foreign value '_indexOf' for purescript module 'Data.String.CodeUnits' not found
 	_ "github.com/purescript-native/go-ffi/purescript-strings"
+	// TODO: remove this one by not using deps that rely on node
+	_ "github.com/purescript-native/go-ffi/purescript-console"
+	_ "github.com/purescript-native/go-ffi/purescript-unsafe-coerce"
+	/*
+		_ "github.com/purescript-native/go-ffi/purescript-random"
+		_ "github.com/purescript-native/go-ffi/purescript-unfoldable"
+	*/
+	// for Foreign value '_indexOf' for purescript module 'Data.String.CodeUnits' not found
 	//	_ "github.com/purescript-native/go-ffi/purescript-assert"
 	//	_ "github.com/purescript-native/go-ffi/purescript-enums"
 	//	_ "github.com/purescript-native/go-ffi/purescript-exceptions"
 	//	_ "github.com/purescript-native/go-ffi/purescript-functions"
-	//	_ "github.com/purescript-native/go-ffi/purescript-lazy"
 	//	_ "github.com/purescript-native/go-ffi/purescript-partial"
 	//	_ "github.com/purescript-native/go-ffi/purescript-refs"
 	//	_ "github.com/purescript-native/go-ffi/purescript-st"
+	. "github.com/purescript-native/go-runtime"
+	"os"
 )
 
 func init() {
+	exports := Foreign("Main")
+	exports["argv"] = func() Any {
+		var args []interface{} = make([]interface{}, len(os.Args))
+		for i, d := range os.Args {
+			args[i] = d
+		}
+		return args
+	}
+
+	exports["exit"] = func(status_ Any) Any {
+		status, _ := status_.(int)
+		os.Exit(status)
+		return nil
+	}
 }
--- main/src/Main.purs
+++ main/src/Main.purs
@@ -1,8 +1,82 @@
 module Main where
 
-import Options.Applicative (execParser, idm, info, strOption, long, Parser, ParserInfo)
 import Prelude
 import Effect (Effect)
+import Options.Applicative.Help (renderHelp)
+import Options.Applicative (Parser, ParserInfo, ParserPrefs, defaultPrefs, execParserPure, idm, info, long, strOption, ParserResult(..), ParserFailure(..), ParserHelp, CompletionResult(..))
+import ExitCodes (ExitCode)
+import ExitCodes as ExitCode
+import Data.Tuple (Tuple(..))
+import Data.Array as Array
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.String as String
+import Data.Newtype (un)
+import Data.Tuple.Nested ((/\))
+import Effect.Console (log)
+import Data.Enum (fromEnum)
+
+foreign import argv :: Effect (Array String)
+
+getArgs :: Effect (Array String)
+-- dropping the program name
+getArgs = argv <#> Array.drop 1
+
+getProgName :: Effect String
+getProgName =
+  argv
+    <#> \args ->
+        fromMaybe "" do
+          executablePath <- Array.index args 1
+          Array.last $ String.split (String.Pattern "/") executablePath
+
+renderFailure :: ParserFailure ParserHelp -> String -> Tuple String ExitCode
+renderFailure failure progn =
+  let
+    h /\ exit /\ cols /\ unit = un ParserFailure failure progn
+  in
+    Tuple (renderHelp cols h) exit
+
+foreign import exit :: forall a. Int -> Effect a
+
+exitWith :: forall void. ExitCode -> Effect void
+exitWith c = exit $ fromEnum c
+
+exitSuccess :: forall a. Effect a
+exitSuccess = exit $ fromEnum ExitCode.Success
+
+handleParseResult :: forall a. ParserResult a -> Effect a
+handleParseResult (Success a) = pure a
+
+handleParseResult (Failure failure) = do
+  progn <- getProgName
+  let
+    Tuple msg exit = renderFailure failure progn
+  {-
+    stream = case exit of
+      ExitCode.Success -> stdout
+      _ -> stderr
+      -}
+  --void $ Stream.writeString stream UTF8 (msg <> "\n") mempty
+  log (msg <> "\n")
+  exitWith exit
+
+
+handleParseResult (CompletionInvoked compl) = do
+  progn <- getProgName
+  msg <- (un CompletionResult compl).execCompletion progn
+  log $ msg
+  exitSuccess
+
+-- void $ Stream.writeString stream UTF8 (msg <> "\n") mempty
+-- exitWith exit
+-- void $ Stream.writeString stdout UTF8 msg mempty
+customExecParser :: forall a. ParserPrefs -> ParserInfo a -> Effect a
+customExecParser pprefs pinfo =
+  execParserPure pprefs pinfo <$> getArgs
+    >>= handleParseResult
+
+execParser :: forall a. ParserInfo a -> Effect a
+execParser = customExecParser defaultPrefs
 
 main :: Effect Unit
 main = execParser opts # void

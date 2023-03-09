6e27a32d808479a840c6ec6220290b8cbfc5788d
Joseph Price <pricejosephd@gmail.com>
2023-03-08 20:47:05 -0500

    upgrade to 0.15

--- shell.nix
+++ shell.nix
@@ -1,1 +1,21 @@
-(import ./default.nix { }).shell
+{ pkgs ? import <nixpkgs> { } }:
+let
+  easy-ps = import
+    (pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "11d3bd58ce6e32703bf69cec04dc7c38eabe14ba";
+      sha256 = "sha256-tESal32bcqqdZO+aKnBzc1GoL2mtnaDtj2y7ociCRGA=";
+    })
+    {
+      inherit pkgs;
+    };
+in
+pkgs.mkShell {
+  buildInputs = [
+    #(import ./default.nix { })
+    easy-ps.purs-0_15_7
+    easy-ps.psc-package
+    easy-ps.spago
+  ];
+}
